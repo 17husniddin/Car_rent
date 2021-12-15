a6690f2b1b4802e2bb27d4bac5a5e3014b959e9e
17husniddin <hmuminov2021@gmail.com>
2021-12-15 16:10:20 +0500

    Vaqt

--- car_date/models.py
+++ car_date/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Car_date(models.Model):
-    date =  models.DateField(auto_now_add=True)
+    date =  models.DateField()
     time_from = models.TimeField()
     time_to = models.TimeField()
     product = models.CharField(max_length=200)
